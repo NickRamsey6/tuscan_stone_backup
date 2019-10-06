3ecf8ef4032745d14d62c627b0b47e3619b20c1d
Nick Ramsey <nramseysc@gmail.com>
2019-10-06 00:30:38 -0700

    landing page view with upload forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'landing#index'
+  resources :stores do
+    collection { post :import}
+  end
+  resources :products do
+    collection { post :import}
+  end
+  resources :employees do
+    collection { post :import}
+  end
 end
