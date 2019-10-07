eacb125ecca981191fd5a48092c0f7639f989df7
Nick Ramsey <nramseysc@gmail.com>
2019-10-07 12:32:05 -0700

    fix gross margin query

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -27,6 +27,9 @@ class StoresController < ApplicationController
   def show
     @store = Store.find(params[:id])
     @todays_sales = ActiveRecord::Base.connection.execute("SELECT SUM(price) FROM products JOIN orders ON (orders.product_id = products.id) WHERE store_id =(#{@store.id});").values[0][0]
+    @todays_labor = ActiveRecord::Base.connection.execute("SELECT SUM(wage*hours) FROM employees JOIN timecards ON(timecards.employee_id = employees.id) WHERE employees.store_id =(#{@store.id});").values[0][0]
+    @todays_cogs = ActiveRecord::Base.connection.execute("SELECT SUM(cost) FROM products JOIN orders ON (orders.product_id = products.id) WHERE store_id =(#{@store.id});").values[0][0]
+    @todays_grossmargin = ActiveRecord::Base.connection.execute("SELECT (SELECT SUM(price) FROM products JOIN orders ON (orders.product_id = products.id)) -( (SELECT SUM(wage*hours) FROM employees JOIN timecards ON(timecards.employee_id = employees.id)) + (SELECT SUM(cost) FROM products JOIN orders ON (orders.product_id = products.id) ))").values[0][0]
     render :show
   end
 
