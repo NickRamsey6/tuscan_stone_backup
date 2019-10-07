28caf584da309d45b59db92ce1622d443b5376ef
Nick Ramsey <nramseysc@gmail.com>
2019-10-07 13:36:52 -0700

    stashing changes before attempting new tutorial

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -29,7 +29,7 @@ class StoresController < ApplicationController
     @todays_sales = ActiveRecord::Base.connection.execute("SELECT SUM(price) FROM products JOIN orders ON (orders.product_id = products.id) WHERE store_id =(#{@store.id});").values[0][0]
     @todays_labor = ActiveRecord::Base.connection.execute("SELECT SUM(wage*hours) FROM employees JOIN timecards ON(timecards.employee_id = employees.id) WHERE employees.store_id =(#{@store.id});").values[0][0]
     @todays_cogs = ActiveRecord::Base.connection.execute("SELECT SUM(cost) FROM products JOIN orders ON (orders.product_id = products.id) WHERE store_id =(#{@store.id});").values[0][0]
-    @todays_grossmargin = ActiveRecord::Base.connection.execute("SELECT (SELECT SUM(price) FROM products JOIN orders ON (orders.product_id = products.id)) -( (SELECT SUM(wage*hours) FROM employees JOIN timecards ON(timecards.employee_id = employees.id)) + (SELECT SUM(cost) FROM products JOIN orders ON (orders.product_id = products.id) ))").values[0][0]
+    @todays_grossmargin = ActiveRecord::Base.connection.execute("SELECT(SELECT SUM(price) FROM products JOIN orders ON (orders.product_id = products.id) WHERE store_id=(#{@store.id})) - ((SELECT SUM(wage*hours) FROM employees JOIN timecards ON(timecards.employee_id = employees.id) WHERE employees.store_id=(#{@store.id})) + (SELECT SUM(cost) FROM products JOIN orders ON (orders.product_id = products.id) WHERE store_id =(#{@store.id})))").values[0][0]
     render :show
   end
 
